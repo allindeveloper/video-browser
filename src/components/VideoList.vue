864c63b8965ff47c2bff01ecbf50702472ecee52
Uchendu Precious <uchendubozz@gmail.com>
2020-02-23 04:00:37 +0100

    initial commit

---
+++ src/components/VideoList.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul class="list-group">
+       <VideoListItem 
+       v-for="video in videos"
+       :video="video"
+       :key="video.etag"/>
+    </ul>
+</template>
+
+
+<script>
+import VideoListItem from "./VideoListItem";
+export default {
+    name:'VideoList',
+    props:['videos'],
+    components:{
+        VideoListItem
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
